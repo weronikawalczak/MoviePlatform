f9cb2a8e6f4ab64e25bb6966346082c2ad1f3f28
weronikawalczak <weronikawalczak989@gmail.com>
2020-04-05 19:08:56 +0200

    Working simple in memory authentication

--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -1,18 +1,37 @@
 package com.example.demo.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
+    protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception{
+        authenticationManagerBuilder.inMemoryAuthentication()
+                .withUser("user1").password(passwordEncoder().encode("12345")).roles("ADMIN");
+    }
+
+    @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception{
         httpSecurity
+                .csrf().disable()
                 .authorizeRequests()
                     .antMatchers(HttpMethod.GET, "/hello").permitAll()
-                .anyRequest().authenticated();
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
